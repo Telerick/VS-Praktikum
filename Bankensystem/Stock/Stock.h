7c62a836a237ff0b3ba6905a230a5cc88a64a73d
telerick <erikberg89@yahoo.de>
2023-04-14 16:18:19 +0200

    fix(add stock data etc.)

---
+++ Bankensystem/Stock/Stock.h
@@ -0,0 +1,43 @@
+//
+// Created by Erik Berg on 12.04.23.
+//
+
+#ifndef BANKENSYSTEM_STOCK_H
+#define BANKENSYSTEM_STOCK_H
+
+#include "string"
+#include "vector"
+#include "random"
+#include <cstdlib>
+#include <ctime>
+
+class Stock {
+public:
+    Stock(std::string acronym, unsigned int price)
+            : acronym(acronym), price(price) {
+    }
+
+    std::string getAcronym() {
+        return this->acronym;
+    }
+
+    void setAcronym(std::string acronym) {
+        this->acronym = acronym;
+    }
+
+    unsigned int getPrice() {
+        return this->price;
+    }
+
+    void setPrice(unsigned int price) {
+        this->price = price;
+    }
+
+
+protected:
+    std::string acronym;
+    unsigned int price;
+};
+
+
+#endif //BANKENSYSTEM_STOCK_H
---
+++ Bankensystem/StockMarket/StockMarket.h
@@ -0,0 +1,53 @@
+//
+// Created by Erik Berg on 12.04.23.
+//
+#include <iostream>
+#include "SMStock.h"
+
+#ifndef BANKENSYSTEM_STOCKMARKET_H
+#define BANKENSYSTEM_STOCKMARKET_H
+
+class StockMarket {
+
+public:
+
+    const std::string getName() {
+        return Name;
+    }
+
+    void setName(const std::string &name) {
+        Name = name;
+    }
+
+    const std::vector<SMStock *> &getStocksOnMarket() {
+        return stocksOnMarket;
+    }
+
+    void initData(){
+        stocksOnMarket.push_back(new SMStock("LSFT", 200));
+        stocksOnMarket.push_back(new SMStock("TC19", 60));
+        stocksOnMarket.push_back(new SMStock("MNSW", 300));
+        stocksOnMarket.push_back(new SMStock("MFC", 180));
+        stocksOnMarket.push_back(new SMStock("GDAG", 120));
+        stocksOnMarket.push_back(new SMStock("TB2", 230));
+        stocksOnMarket.push_back(new SMStock("JOI", 90));
+    }
+
+    void printStockMarket(){
+        for (int i = 0; i < stocksOnMarket.size(); ++i) {
+            std::cout << stocksOnMarket[i]->getAcronym() << std::endl;
+        }
+    }
+
+    std::string generateTransaction(){
+        int index = rand()%stocksOnMarket.size();
+        return stocksOnMarket[index]->createRandomizedStockMessage();
+    }
+
+private:
+    std::string Name;
+    std::vector<SMStock *> stocksOnMarket;
+};
+
+
+#endif //BANKENSYSTEM_STOCKMARKET_H
