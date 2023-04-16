859c3211011247912f561f32b33ce2a01b698614
tobias.pletsch <tobias.pletsch@telekom.de>
2023-04-16 17:57:22 +0200

    fix merge

--- Bankensystem/Bank/bank.cpp
+++ Bankensystem/Bank/bank.cpp
@@ -7,13 +7,8 @@
 
 int main() {
     srand(time(0));
-<<<<<<< HEAD
     // Bank bank(fillPortfolio(), bankNames[rand() % 30], 8080);
-    Bank bank(fillPortfolio(), std::getenv("CONTAINER_NAME"), 8080);
-=======
-    Bank bank(fillPortfolio(), getRandomBankName());
-    bank.registerToStockMarket();
->>>>>>> 6083334fe2c6e5bfc61bef16b717d3c9698d1a9d
+    Bank bank(fillPortfolio(), std::getenv("CONTAINER_NAME"));
     bank.receiveMessage();
     std::cout << "OUTSIDE receiveMessage()" << std::endl;
     
